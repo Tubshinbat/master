25764a8b8eb5b04fc641e42308bb865f0b4db3d3
Tubshinbat~ <tuvshinbat596@gmail.com>
2023-10-04 04:13:39 +0800

    a

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import "aos/dist/aos.css";
 import Script from "next/script";
 import Header from "components/Generals/Header";
 import Footer from "components/Generals/Footer";
-import { Suspense, useEffect } from "react";
+import { Suspense } from "react";
 import Loader from "components/Generals/Loader";
 import { ToastContainer } from "react-toastify";
 
@@ -17,35 +17,11 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  const googleTranslateElementInit = () => {
-    new window.google.translate.TranslateElement(
-      {
-        pageLanguage: "en",
-        autoDisplay: false,
-      },
-      "google_translate_element"
-    );
-  };
-  useEffect(() => {
-    var addScript = document.createElement("script");
-    addScript.setAttribute(
-      "src",
-      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-    );
-    document.body.appendChild(addScript);
-    window.googleTranslateElementInit = googleTranslateElementInit;
-  }, []);
-
   return (
     <html lang="en">
       <body>
         <Suspense fallback={<Loader />}>
           <Header />
-          <div className="container">
-            <div className="translate-google">
-              <div id="google_translate_element"></div>
-            </div>
-          </div>
           {children}
           <Footer />
           <ToastContainer
--- app/page.js
+++ app/page.js
@@ -1,14 +1,40 @@
+"use client";
 import Loader from "components/Generals/Loader";
 import Side from "components/Generals/Side";
 import MemberList from "components/MemberList";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 
 export default function Page() {
+  const googleTranslateElementInit = () => {
+    new window.google.translate.TranslateElement(
+      {
+        pageLanguage: "en",
+        autoDisplay: false,
+      },
+      "google_translate_element"
+    );
+  };
+  useEffect(() => {
+    var addScript = document.createElement("script");
+    addScript.setAttribute(
+      "src",
+      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+    );
+    document.body.appendChild(addScript);
+    window.googleTranslateElementInit = googleTranslateElementInit;
+  }, []);
+
   return (
     <>
       <Suspense fallback={<Loader />}>
         <div className="main">
           <section>
+            <div className="container">
+              <div className="translate-google">
+                <div id="google_translate_element"></div>
+              </div>
+            </div>
+
             <div className="container">
               <div className="row">
                 <div className="col-lg-3">
