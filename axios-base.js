c475efbb8109b86ed03da52be4e2178e1b7d686f
Tubshinbat~ <tuvshinbat596@gmail.com>
2023-10-09 15:48:52 +0800

    a

--- axios-base.js
+++ axios-base.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://node.mn/api/",
-  // baseURL: "http://localhost:8000/api/v1/",
+  // baseURL: "http://localhost:1003/api/v1/",
 });
 
 instance.defaults.withCredentials = true;
