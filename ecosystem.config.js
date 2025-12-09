aea4fd9a96eeb4bceb86da93ee637942edfd85f0
Tubshinbat <tuvshinbat596@gmail.com>
2025-12-09 15:51:38 +0800

    Rename app from master-cms-portal to topmaster-cms-portal-2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: "master-cms-portal",
+      name: "topmaster-cms-portal-2",
       script: "npm start",
       autorestart: true,
       env: {
-        PORT: 4003,
+        PORT: 4004,
         NODE_ENV: "production",
         SERVER_ENV: "production",
         DEBUG: "server:*",
